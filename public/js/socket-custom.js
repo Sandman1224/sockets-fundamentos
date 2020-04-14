ea797b889360ac0907bd65b7084db3ad4d4ad7ba
Martìn Ganami <martinganami@gmail.com>
2020-04-14 08:40:33 -0300

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+// Escuchar información
+socket.on('connect', function () {
+    console.log('Conectado al servidor');
+});
+
+// Escuchar información
+socket.on('disconnect', function () {
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Martín',
+    mensaje: 'Hola mundo'
+}, function (resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function (mensaje) {
+    console.log('Servidor:', mensaje);
+});
